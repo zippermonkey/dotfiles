c371e0d087abea5efad0e2ee58b81fcec3b7c515
zippermonkey <1960727314@qq.com>
2025-09-06 22:59:11 +0800

    download zsh plugin tool

---
+++ zsh/download-plugins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PLUGIN_DIR=./.zsh
+mkdir -p $PLUGIN_DIR
+
+pushd ${PLUGIN_DIR}
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions 
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+# zsh-autocompletions
+git clone https://github.com/zsh-users/zsh-completions 
+# fzf-tab
+git clone https://github.com/Aloxaf/fzf-tab
+
+popd
